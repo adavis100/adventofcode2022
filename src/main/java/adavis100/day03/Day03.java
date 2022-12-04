0443ca915e64c9048ea920c73922cc2cac8ad45a
Aaron Davis <adavis100@gmail.com>
2022-12-03 21:33:42 -0800

    Clean up day 3

--- src/main/java/adavis100/day03/Day03.java
+++ src/main/java/adavis100/day03/Day03.java
@@ -1,70 +1,52 @@
 package adavis100.day03;
 
-import adavis100.day02.Day02;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day03 {
-    public int getPriorities(String []lines) {
-        int p = 0;
-        for (String s : lines) {
-            String a = s.substring(0, s.length()/2);
-            String b = s.substring(s.length()/2);
-            Set<Character> aItems = new HashSet<>();
-            for (char c : a.toCharArray()) {
-                aItems.add(c);
-            }
-            for (char c : b.toCharArray()) {
-                if (aItems.contains(c)) {
-                    p += getPriority(c);
-                    break;
-                }
-            }
-        }
-        return p;
+    public int getPriorities(List<String> lines) {
+        return lines.stream()
+                .map(line -> getCommonItems(line.substring(0, line.length() / 2), line.substring(line.length() / 2)))
+                .mapToInt(this::getPriority)
+                .sum();
+    }
+
+    private char getCommonItems(String... part) {
+        var sets = Arrays.asList(part).stream()
+                .map(s -> s.chars().mapToObj(c -> (char) c).collect(Collectors.toSet()))
+                .collect(Collectors.toList());
+        var common = sets.stream().skip(1)
+                .collect(() -> new HashSet<>(sets.get(0)), Set::retainAll, Set::retainAll);
+        return common.stream().findFirst().get();
     }
 
     private int getPriority(char c) {
         if (Character.isLowerCase(c)) {
             return 1 + c - 'a';
         } else {
-            return 27 + (c - 'A');
+            return 27 + c - 'A';
         }
     }
 
-    public int getPrioritiesPart2(String[] lines) {
-        int p = 0;
-        for (int i = 0; i < lines.length; i+=3) {
-            String a = lines[i], b = lines[i+1], c = lines[i + 2];
-            Set<Character> aItems = new HashSet<>();
-            for (char ch : a.toCharArray()) {
-                aItems.add(ch);
-            }
-            Set<Character> bItems = new HashSet<>();
-            for (char ch : b.toCharArray()) {
-                bItems.add(ch);
-            }
-            for (char ch : c.toCharArray()) {
-                if (aItems.contains(ch) && bItems.contains(ch)) {
-                    p += getPriority(ch);
-                    break;
-                }
-            }
-        }
-        return p;
+    public int getPrioritiesPart2(List<String> lines) {
+        return IntStream.iterate(0, i -> i < lines.size(), i -> i += 3)
+                .mapToObj(i -> getCommonItems(lines.get(i), lines.get(i + 1), lines.get(i + 2)))
+                .mapToInt(this::getPriority)
+                .sum();
     }
 
     public static void main(String[] args) throws IOException {
         var inStr = Files.readString(Path.of("src/main/resources/day03.txt"));
-        var lines = inStr.split("\n");
+        var lines = Arrays.asList(inStr.split("\n"));
         Day03 day03 = new Day03();
         System.out.println("part 1: " + day03.getPriorities(lines));
         System.out.println("part 2: " + day03.getPrioritiesPart2(lines));
     }
-
 }
--- src/test/java/adavis100/day03/Day03Test.java
+++ src/test/java/adavis100/day03/Day03Test.java
@@ -2,15 +2,17 @@ package adavis100.day03;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day03Test {
-    private String []lines = new String[] {"vJrwpWtwJgWrhcsFMMfFFhFp",
+    private List<String> lines = List.of("vJrwpWtwJgWrhcsFMMfFFhFp",
             "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
             "PmmdzqPrVvPwwTWBwg",
             "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
             "ttgJtRGJQctTZtZT",
-            "CrZsJsPPZsGzwwsLwLmpwMDw"};
+            "CrZsJsPPZsGzwwsLwLmpwMDw");
 
     @Test
     void solvesExample() {
