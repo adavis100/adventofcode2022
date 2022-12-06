47f68a3e94dd3a81cb5e3fd51fe18df4bcc4f1c0
Aaron Davis <adavis100@gmail.com>
2022-12-06 07:24:15 -0800

    Refactor day 6

--- src/main/java/adavis100/day06/Day06.java
+++ src/main/java/adavis100/day06/Day06.java
@@ -1,29 +1,20 @@
 package adavis100.day06;
 
-import adavis100.day05.Day05;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day06 {
     static int getMarkerCount(String s, int sequenceLength) {
-        Map<Character,Integer> seen = new HashMap<>();
-        int i;
-        for (i = 0; i < sequenceLength; i++) {
-            seen.put(s.charAt(i), seen.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        for (; !allUnique(seen) && i < s.length(); i++) {
-            seen.put(s.charAt(i - sequenceLength), seen.get(s.charAt(i - sequenceLength)) - 1);
-            seen.put(s.charAt(i), seen.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        return i;
+        return IntStream.range(sequenceLength, s.length())
+                .filter(i -> allUnique(s.substring(i - sequenceLength, i)))
+                .findFirst().orElse(0);
     }
 
-    private static boolean allUnique(Map<Character, Integer> map) {
-        return map.values().stream().noneMatch(it -> it > 1);
+    private static boolean allUnique(String s) {
+        return s.chars().boxed().collect(Collectors.toSet()).size() == s.length();
     }
 
     public static void main(String[] args) throws IOException {
