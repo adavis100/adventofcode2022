8a147f58bc0438c767b0332132608d3d4fa0a8d5
Aaron Davis <adavis100@gmail.com>
2022-12-11 23:40:23 -0800

    Add day 12 part 2

--- src/main/java/adavis100/day12/Day12.java
+++ src/main/java/adavis100/day12/Day12.java
@@ -4,17 +4,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Day12 {
     record Position(int x, int y) {}
 
     final Position marker = new Position(-1, -1);
-    public int getShortestPathLength(String in) {
+    public int getShortestPathLengthPart1(String in) {
         char[][] grid = loadGrid(in);
-        Set<Position> visited = new HashSet<>();
         Position start = findStart(grid);
         Position end = findEnd(grid);
+        return findShortestPathFrom(grid, start, end);
+    }
+
+    public int getShortestPathLengthPart2(String in) {
+        char[][] grid = loadGrid(in);
+        List<Position> starts = findAllPossibleStarts(grid);
+        Position end = findEnd(grid);
+        return starts.stream().mapToInt(start -> findShortestPathFrom(grid, start, end)).min().orElseThrow();
+    }
+
+    private List<Position> findAllPossibleStarts(char[][] grid) {
+        var list = new ArrayList<Position>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                if (grid[row][col] == 'a' || grid[row][col] == 'S') {
+                    grid[row][col] = 'a';
+                    list.add(new Position(row, col));
+                }
+            }
+        }
+        return list;
+    }
+
+    private int findShortestPathFrom(char[][] grid, Position start, Position end) {
+        Set<Position> visited = new HashSet<>();
         grid[start.x][start.y] = 'a';
         grid[end.x][end.y] = 'z';
         Deque<Position> queue = new ArrayDeque<>();
@@ -27,10 +52,12 @@ public class Day12 {
             Position cur = queue.remove();
             if (cur.equals(marker)) {
                 dist++;
-                queue.add(marker);
+                if (!queue.isEmpty()) {
+                    queue.add(marker);
+                }
                 continue;
             } else if (cur.equals(end)) {
-                break;
+                return dist;
             }
             for (Position pos : getNeighbors(cur, grid)) {
                 if (!visited.contains(pos)) {
@@ -39,7 +66,7 @@ public class Day12 {
                 }
             }
         }
-        return dist;
+        return Integer.MAX_VALUE;
     }
 
     private List<Position> getNeighbors(Position cur, char[][] grid) {
@@ -99,7 +126,8 @@ public class Day12 {
     public static void main(String[] args) throws IOException {
         var inStr = Files.readString(Path.of("src/main/resources/day12.txt"));
         Day12 day12 = new Day12();
-        System.out.println("part 1: " + day12.getShortestPathLength(inStr));
-//        System.out.println("part 2: " + day12.simulateMovesPart2(lines));
+        System.out.println("part 1: " + day12.getShortestPathLengthPart1(inStr));
+        System.out.println("part 2: " + day12.getShortestPathLengthPart2(inStr));
     }
+
 }
--- src/test/java/adavis100/day12/Day12Test.java
+++ src/test/java/adavis100/day12/Day12Test.java
@@ -2,21 +2,26 @@ package adavis100.day12;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day12Test {
-    @Test
-    void solvesExample1() {
-        String in = """
+    String in = """
                 Sabqponm
                 abcryxxl
                 accszExk
                 acctuvwj
                 abdefghi""";
-
+    @Test
+    void solvesExample1() {
         Day12 day12 = new Day12();
-        int moves = day12.getShortestPathLength(in);
+        int moves = day12.getShortestPathLengthPart1(in);
         assertThat(moves).isEqualTo(31);
+    }
+
+    @Test
+    void solvesExample2() {
+        Day12 day12 = new Day12();
+        int moves = day12.getShortestPathLengthPart2(in);
+        assertThat(moves).isEqualTo(29);
     }
 }
