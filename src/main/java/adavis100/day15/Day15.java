299ffefe04e50f543c7ad57fcf0428a87a46fdc1
Aaron Davis <adavis100@gmail.com>
2022-12-15 08:41:46 -0800

    Add day15 part 2

--- src/main/java/adavis100/day15/Day15.java
+++ src/main/java/adavis100/day15/Day15.java
@@ -16,11 +16,11 @@ public class Day15 {
         int maxX = findMaxX(sensors) + 20000000;
         int count = 0;
         for (int x = minX; x <= maxX; x++) {
-            var pos = new Coord(x,y);
+            var coord = new Coord(x,y);
             for (Sensor sensor : sensors) {
-                if (pos.equals(sensor.closestBeacon)) {
+                if (coord.equals(sensor.closestBeacon)) {
                     break;
-                } else if (dist(sensor.position, pos) <= dist(sensor.position, sensor.closestBeacon)) {
+                } else if (dist(sensor.position, coord) <= dist(sensor.position, sensor.closestBeacon)) {
                     count++;
                     break;
                 }
@@ -56,9 +56,70 @@ public class Day15 {
         return sensors.stream().mapToInt(i -> i.closestBeacon.x).max().orElseThrow();
     }
 
+    static long solvePart2(String in, int maxCoord) {
+        List<Sensor> sensors = loadSensors(in);
+        long tuningFreq = 0;
+        for (int y = 0; y < maxCoord; y++) {
+            tuningFreq = findBeacon(sensors, y, maxCoord);
+            if (tuningFreq != 0) {
+                break;
+            }
+        }
+        return tuningFreq;
+    }
+
+    private static long findBeacon(List<Sensor> sensors, int row, int maxCol) {
+        for (Sensor sensor : sensors) {
+            if (sensorHitsRow(sensor, row)) {
+               int xmin = getSensorMinCol(sensor, row);
+               int xmax = getSensorMaxCol(sensor, row);
+               var minCoord = new Coord(xmin - 1, row);
+                var maxCoord = new Coord(xmax + 1, row);
+               if (xmin - 1 > 0 && !detected(sensors, minCoord)) {
+                   return tuningFrequency(minCoord);
+               }
+               if (xmax + 1 <= maxCol && !detected(sensors, maxCoord)) {
+                    return tuningFrequency(maxCoord);
+               }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean detected(List<Sensor> sensors, Coord coord) {
+        for (Sensor sensor : sensors) {
+            if (dist(sensor.position, coord) <= dist(sensor.position, sensor.closestBeacon)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean sensorHitsRow(Sensor sensor, int row) {
+        Coord coord = new Coord(sensor.position.x, row);
+        return sensor.position.y == row || dist(sensor.position, coord) <= dist(sensor.position, sensor.closestBeacon);
+    }
+
+    private static int getSensorMinCol(Sensor sensor, int row) {
+        int dist = dist(sensor.position, sensor.closestBeacon);
+        dist -= Math.abs(sensor.position.y - row);
+        return sensor.position.x - dist;
+    }
+
+    private static int getSensorMaxCol(Sensor sensor, int row) {
+        int dist = dist(sensor.position, sensor.closestBeacon);
+        dist -= Math.abs(sensor.position.y - row);
+        return sensor.position.x + dist;
+    }
+
+
+    private static long tuningFrequency(Coord coord) {
+        return coord.x * 4000000L + coord.y;
+    }
+
     public static void main(String[] args) throws IOException {
         var inStr = Files.readString(Path.of("src/main/resources/day15.txt"));
         System.out.println("part 1: " + solvePart1(inStr, 2000000));
-//        System.out.println("part 2: " + solvePart2(inStr));
+        System.out.println("part 2: " + solvePart2(inStr, 4000000));
     }
 }
